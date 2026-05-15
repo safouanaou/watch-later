68314c23f1eaace8f6f494f532ae816cd34085d0
safouanaou <aouezgharsafouan@gmail.com>
2026-05-15 18:02:15 +0200

    Add Bootstrap for styling and enhance Login component layout
    - Integrated Bootstrap 5.3.8 for improved styling.
    - Updated Login component structure to utilize Bootstrap classes for better responsiveness.
    - Added new CSS styles for the login page background and form aesthetics.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,14 +3,23 @@ import "../styles/login.css";
 
 export default function Login() {
     return (
-        <div className="login-page">
-            <h1>Login</h1>
-            <div className="login-container">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email"/>
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password"/>
+        <div className="login-page container min-vh-100 d-flex justify-content-center align-items-center">
+
+                <section className="login-form container-fluid d-flex align-items-center flex-column">
+
+                    <h1>Login</h1>
+                    <form action="" method="POST" className="form container d-flex justify-content-center align-items-center flex-column">
+                            <input type="email" id="email" placeholder="email"/> 
+                            <input type="password" id="password" placeholder="password"/>
+                        
+                        <button id="submit-button" type="submit">Login</button>
+                    </form>
+
+                </section>
+
+
             </div>
-        </div>
+
+            
     )
 }
